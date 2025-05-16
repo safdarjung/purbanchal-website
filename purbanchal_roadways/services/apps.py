9fd7475962b6d20f29775c5f185fa370cb04981d
Safdar <safdarxdxd@gmail.com>
2025-05-16 17:31:09 +0530

    Initial commit

--- purbanchal_roadways/services/apps.py
+++ purbanchal_roadways/services/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'purbanchal_roadways.services'
+    name = 'services'
