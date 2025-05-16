03cadc7b12e979caaf813f0885436671292343d0
Safdar <safdarxdxd@gmail.com>
2025-05-16 16:32:33 +0530

    updated app.py

--- purbanchal_roadways/purbanchal_roadways_main/settings.py
+++ purbanchal_roadways/purbanchal_roadways_main/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'purbanchal_roadways.core',
-    'purbanchal_roadways.services',
-    'purbanchal_roadways.contact',
+    'core',
+    'services',
+    'contact',
 ]
 
 MIDDLEWARE = [
