b60dccc1c7f484813d123ffb77470ac5248abd0e
Safdar <safdarxdxd@gmail.com>
2025-05-17 05:42:27 +0530

    Add STATICFILES_DIRS to fix static files 404 on deployment

--- purbanchal_roadways_main/settings.py
+++ purbanchal_roadways_main/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 STATIC_URL = env.str('STATIC_URL', default='/static/')
 # STATIC_ROOT = env.str('STATIC_ROOT', default=os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 WHITENOISE_AUTOREFRESH = DEBUG
 WHITENOISE_USE_FINDERS = True
 
